4f32a75e373a23eed1f1fffe124669cba752c190
Christian Hustert <christianhustert@gmail.com>
2022-02-22 12:38:40 +1300

    forecast for next four days working now

--- scripts.js
+++ scripts.js
@@ -1,19 +1,16 @@
-// OpenWeatherMap API. Do not share it publicly.
-const api = 'e342256da9825740d4271c494cb88603'; //Replace with your API
+const api = 'e342256da9825740d4271c494cb88603'; // openweathermap API key
 
 const iconImg = document.getElementById('weather-icon');
 const loc = document.querySelector('#location');
 const tempC = document.querySelector('.c');
 const tempF = document.querySelector('.f');
+const dateDOM = document.querySelector('.date');
 const desc = document.querySelector('.desc');
 const humidityDOM = document.querySelector('.humidity');
 const windSpeedDOM = document.querySelector('.windSpeed');
 const windDirectionDOM = document.querySelector('.windDirection');
 const sunriseDOM = document.querySelector('.sunrise');
 const sunsetDOM = document.querySelector('.sunset');
-
-const dayNameDOM = document.querySelector('.day1');
-const tempCDaily = document.querySelector('.day1-temp');
 
 window.addEventListener('load', () => {
   let long;
@@ -22,11 +19,10 @@ window.addEventListener('load', () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       // Storing Longitude and Latitude in variables
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
+      long = '174.8858'; //position.coords.longitude ----- Raglan: 174.8992
+      lat =  '-37.7968'; //position.coords.latitude ----- Raglan: -37.830656
       const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
       const forecast_base = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly,alerts&appid=${api}&units=metric`
-
 
       // Using fetch to get data
       fetch(base)
@@ -36,6 +32,7 @@ window.addEventListener('load', () => {
         .then((data) => {
 	        const { temp } = data.main;
 	        const place = data.name;
+	        const date = data.dt;
 	        const { description, icon } = data.weather[0];
 	        const { sunrise, sunset } = data.sys;
 	        const { humidity } = data.main;
@@ -48,6 +45,7 @@ window.addEventListener('load', () => {
 	        // Converting Epoch(Unix) time to GMT
 	        const sunriseGMT = new Date(sunrise * 1000);
 	        const sunsetGMT = new Date(sunset * 1000);
+	        const dateGMT = new Date(date * 1000);
 
 	        // const windDirection = deg;
 
@@ -59,16 +57,15 @@ window.addEventListener('load', () => {
 
 			const windDirection = degToCompass(deg);
 
-
-
 	        // Interacting with DOM to show data
 	        iconImg.src = iconUrl;
 	        loc.textContent = `${place}`;
+	        dateDOM.textContent = `${dateGMT.toLocaleDateString('en-NZ',{ day: '2-digit', month: '2-digit', year: '2-digit' })}`;
 	        desc.textContent = `${description}`;
-	        tempC.textContent = `${temp.toFixed(2)} °C`;
-	        tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
-	        sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
-	        sunsetDOM.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
+	        tempC.textContent = `${temp.toFixed(0)}°C`;
+	        tempF.textContent = `${fahrenheit.toFixed(0)}°F`;
+	        sunriseDOM.textContent = `${sunriseGMT.toLocaleTimeString('en-NZ', { hour: '2-digit', minute: '2-digit' })}`;
+	        sunsetDOM.textContent = `${sunsetGMT.toLocaleTimeString('en-NZ',{ hour: '2-digit', minute: '2-digit' })}`;
 	        humidityDOM.textContent = `${humidity}%`;
 	        windSpeedDOM.textContent = `${speed} m/s`;
 	        windDirectionDOM.textContent = `${windDirection}`;
@@ -79,19 +76,32 @@ window.addEventListener('load', () => {
         		return response.json();
         	})
         	.then((data) => {
-        		const { dt } = data.daily[1];
-        		const { day } =data.daily[1].temp;
 
-        		function getDayName(dt) {
-        			const dtGMT = new Date(dt * 1000);
-        			var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        			return days[dtGMT.getDay()];
+        		for (var i=1; i <= 4; i++) {
+        			// Identify next four days and loop through
+        			const dayNameDOM = document.querySelector('.day' + i);
+	        		const { dt } = data.daily[i];
+
+	        		const dtGMT = new Date(dt * 1000);
+	        		var days = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+
+	        		const dayName = days[dtGMT.getDay()];
+	        		
+	        		// function getDayName(dt) {
+	        		// 	const dtGMT = new Date(dt * 1000);
+	        		// 	var days = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+	        		// 	return days[dtGMT.getDay()];
+	        		// }
+	        		// const dayName = getDayName(dt);
+
+	        		dayNameDOM.textContent = `${dayName}`;
+
+	        		// Identify temperature of next four days and loop through
+        			const { day } = data.daily[i].temp;
+        			const tempCDaily = document.querySelector('.day' + i + '-temp');
+        			
+        			tempCDaily.textContent = `${day.toFixed(0)}°C`;
         		}
-
-        		const dayName = getDayName(dt);
-
-        		dayNameDOM.textContent = `${dayName}`;
-        		tempCDaily.textContent = `${day.toFixed(2)} °C`;
         	});
     });
   }
